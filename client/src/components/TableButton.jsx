c717785c0b9108b34f4e81aa7fd3c35d8a8bfc3b
yerayboix <yerayboix@gmail.com>
2024-09-25 18:08:22 +0200

    Eliminada la parte de react para solo generar un servidor

--- client/src/components/TableButton.jsx
+++
@@ -1,5 +0,0 @@
-export function TableButton ({ className, textButton, iconSVG, filter, onClick }) {
-  return (
-    <button className={className} onClick={onClick}><img src={iconSVG} style={{ width: 'auto', height: '23px', filter, paddingBottom: '2px' }} /> {textButton}</button>
-  )
-}
--- client/src/pages/BookPage.jsx
+++
@@ -1,7 +0,0 @@
-import { BookList } from '../components/BookList'
-
-export function BookPage () {
-  return (
-    <BookList />
-  )
-}
